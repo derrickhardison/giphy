a81facfbcf3563e0b4998593c53c89459fb58807
dlhardi <derricklhardison@gmail.com>
2021-02-18 11:30:28 -0500

    Added three pages/containers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Random from "./Containers/Random/Random";
+import Search from "./Containers/Search/Search";
+import Trending from "./Containers/Trending/Trending";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Random />
+      <Trending />
+      <Search />
     </div>
   );
 }
---
+++ src/Containers/Trending/Trending.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Trending = () => {
+    return (
+        <div>
+            <h1>This is the trending page</h1>
+        </div>
+    );
+};
+
+export default Trending;
