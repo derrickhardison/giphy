24de31ca0009092ea377ced0e47307f37e6166eb
dlhardi <derricklhardison@gmail.com>
2021-02-18 11:37:39 -0500

    Installed react-router-dom. Imported HashRouter and Router. Set up client side routing.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Random from "./Containers/Random/Random";
 import Search from "./Containers/Search/Search";
 import Trending from "./Containers/Trending/Trending";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Random />
-      <Trending />
-      <Search />
+      <Router>
+        <Route exact path="/" component={Random} />
+        <Route path="/trending" component={Trending} />
+        <Route path="/search" component={Search} />
+      </Router>
     </div>
   );
 }
